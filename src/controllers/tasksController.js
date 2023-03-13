18b02b00491a75b4b8d8c66ad825afd114a2faf0
Matheus-Marti1 <30555147+Matheus-Marti1@users.noreply.github.com>
2023-03-13 18:55:55 -0300

    first commit

---
+++ src/controllers/tasksController.js
@@ -0,0 +1,40 @@
+const tasksModel = require('../models/tasksModel');
+
+const getAll = async (_request, res) => {
+  const tasks = await tasksModel.getAll();
+  return res.status(200).json(tasks);
+};
+
+const getById = async (request, response) => {
+  const { id } = request.params;
+
+  const tasks = await tasksModel.getById(id);
+  return response.status(200).json(tasks);
+};
+
+const createTask = async (request, response) => {
+  const createdTask = await tasksModel.createTask(request.body);
+  return response.status(201).json(createdTask);
+};
+
+const deleteTask = async (request, response) => {
+  const { id } = request.params;
+
+  await tasksModel.deleteTask(id);
+  return response.status(204).json();
+};
+
+const updateTask = async (request, response) => {
+    const { id } = request.params;
+
+    await tasksModel.updateTask(id, request.body);
+    return response.status(204).json();
+};
+
+module.exports = {
+  getAll,
+  getById,
+  createTask,
+  deleteTask,
+  updateTask,
+};
---
+++ src/router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const tasksController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware')
+
+router.get('/tasks', tasksController.getAll);
+router.get('/tasks/:id', tasksController.getById);
+router.post('/tasks', tasksMiddleware.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id', 
+    tasksMiddleware.validateFieldTitle, 
+    tasksMiddleware.validateFieldStatus, 
+    tasksController.updateTask
+);
+
+module.exports = router;
